b758009d68ca9d2733cd98e290d827dd11082b72
Fozevi <ivezof@proton.me>
2024-06-10 20:57:50 +0300

    msg add

---
+++ resources/views/layouts/message.blade.php
@@ -0,0 +1,11 @@
+<div class="position-fixed bottom-5 end-0 p-3" style="z-index: 11" id="divMsg">
+    <div id="msg" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="5000">
+        <div class="toast-header">
+            <strong class="me-auto"></strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Закрыть"></button>
+        </div>
+        <div class="toast-body">
+
+        </div>
+    </div>
+</div>
